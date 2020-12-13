2e0f19fb355babcf071d574ff7640e78bb7e5aa3
ThemeBing <themebing@gmail.com>
2020-12-13 15:53:11 +0600

    update

--- inc/demo-importer/theme-demo.php
+++ inc/demo-importer/theme-demo.php
@@ -52,41 +52,34 @@ class ThemeBing_Install_Themes {
 		<div class="themes-showcase wrap">
 
 			<h2 class="wp-heading-inline"><?php esc_attr_e('Themes from ThemeBing', 'woovina-sites'); ?></h2>
-			<ul class="theme-filter-links">
-			
+			<div class="theme-filter">
+				<ul class="theme-filter-links">
+					<li class="current"><a href="#">All</a></li>
 
-			<?php $data = wp_remote_retrieve_body( wp_remote_get( 'https://raw.githubusercontent.com/ThemeBing/theme-demos/main/data.json' ));
+				<?php $data = wp_remote_retrieve_body( wp_remote_get( 'https://raw.githubusercontent.com/ThemeBing/theme-demos/main/data.json' ));
 
-			$themes = json_decode( $data , true );
-			if ($themes) {
-				foreach ($themes as $key => $theme) {
-					var_dump($theme);
-				}
-			}
-			 ?>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-				<li><a href="#">All</a></li>
-			</ul>
-			<div clas="themebing-search">
-				<input type="text" class="themebing-search-input" name="themebing-search" value="" placeholder="<?php esc_html_e('Search themes...', 'tb-themes'); ?>">
+				$themes = json_decode( $data , true );
+				if ($themes) {
+					foreach ($themes['categories'] as $key => $category) { ?>
+						<li><a href="#" data-cat="<?php echo esc_attr( strtolower($category) ) ?>"><?php echo esc_html( $category ) ?></a></li>
+					<?php }
+				} ?>
+				</ul>
+				<div class="themebing-search">
+					<input type="text" class="themebing-search-input" name="themebing-search" value="" placeholder="<?php esc_html_e('Search themes...', 'tb-themes'); ?>">
+				</div>
 			</div>
 			<div class="theme-browser rendered">
 
 				<div class="themes wp-clearfix">
 					<?php 
 					if ($themes) {
-						foreach ($themes as $key => $theme) { ?>
+						foreach ($themes['products'] as $key => $theme) { ?>
 						<div class="theme" tabindex="0" aria-describedby="twentytwentyone-action twentytwentyone-name" data-slug="twentytwentyone">
 							<div class="theme-screenshot">
 								<img src="<?php echo esc_url( $theme["image"] ) ?>" alt="<?php echo esc_attr( $theme["name"] ) ?>">
 							</div>
-							
+							<span class="more-details"><?php echo esc_html( $theme["name"] ) ?></span>
 							<div class="theme-author">By WordPress.org</div>
 							<div class="theme-id-container">
 								<h3 class="theme-name"><?php echo esc_html( $theme["name"] ) ?></h3>
