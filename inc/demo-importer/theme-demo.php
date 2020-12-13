44791a5a71c2be666be208e166caef2ad33e98b4
ThemeBing <themebing@gmail.com>
2020-12-13 14:13:34 +0600

    Update theme-demo.php

--- inc/demo-importer/theme-demo.php
+++ inc/demo-importer/theme-demo.php
@@ -25,50 +25,46 @@ function enqueue_scripts() {
 
 add_action('admin_enqueue_scripts', 'enqueue_scripts');
 
-function get_themes_data() {
+$data = array(
+	'Azim' => array(
+		'categories'        => array('FREE', 'Clothing Fashion', 'Electronics', 'Furniture'),
+		'demo_class'        => 'free-demo',
+		'xml_file'     		=> $dir . 'azim/content.xml',
+		'theme_settings' 	=> $dir . 'azim/customizer.json',
+		'widgets_file'  	=> $dir . 'azim/widgets.wie',
+		'form_file'  		=> $dir . 'azim/redux.json',
+		'preview_image'		=> $dir . 'azim/demo.jpg',
+		'preview_url'		=> 'https://themebing.com/wp/azim/',
+		'required_plugins'  => array(
+			'free' => array(
+				array(
+					'slug'  	=> 'elementor',
+					'init'  	=> 'elementor/elementor.php',
+					'name'  	=> 'Elementor',
+				),
+				array(
+					'slug'  	=> 'woocommerce',
+					'init'  	=> 'woocommerce/woocommerce.php',
+					'name'  	=> 'WooCommerce',
+				),
+				array(
+					'slug'  	=> 'wpforms-lite',
+					'init'  	=> 'wpforms-lite/wpforms.php',
+					'name'  	=> 'WPForms',
+				),
+			)
+		),
+	), 
+);
 
-	$dir = plugins_url( '/', __FILE__ );
 
-	$data = array(
-		'Azim' => array(
-			'categories'        => array('FREE', 'Clothing Fashion', 'Electronics', 'Furniture'),
-			'demo_class'        => 'free-demo',
-			'xml_file'     		=> $dir . 'azim/content.xml',
-			'theme_settings' 	=> $dir . 'azim/customizer.json',
-			'widgets_file'  	=> $dir . 'azim/widgets.wie',
-			'form_file'  		=> $dir . 'azim/redux.json',
-			'preview_image'		=> $dir . 'azim/demo.jpg',
-			'preview_url'		=> 'https://themebing.com/wp/azim/',
-			'required_plugins'  => array(
-				'free' => array(
-					array(
-						'slug'  	=> 'elementor',
-						'init'  	=> 'elementor/elementor.php',
-						'name'  	=> 'Elementor',
-					),
-					array(
-						'slug'  	=> 'woocommerce',
-						'init'  	=> 'woocommerce/woocommerce.php',
-						'name'  	=> 'WooCommerce',
-					),
-					array(
-						'slug'  	=> 'wpforms-lite',
-						'init'  	=> 'wpforms-lite/wpforms.php',
-						'name'  	=> 'WPForms',
-					),
-				)
-			),
-		), 
-	);
-}
-
-function create_admin_theme_page() { ?>
+function create_admin_theme_page($data) { ?>
 
 	<div class="themes-showcase wrap">
 
 		<h2 class="wp-heading-inline"><?php esc_attr_e('Themes from ThemeBing', 'woovina-sites'); ?></h2>
 		<ul class="theme-filter-links">
-			<?php print_r(get_themes_data()) ?>
+			<?php print_r( $data ); ?>
 			<li><a href="#">All</a></li>
 			<li><a href="#">All</a></li>
 			<li><a href="#">All</a></li>
