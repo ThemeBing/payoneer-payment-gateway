8a4a351b29fffbd917a902d4c14a9f01dc3d6205
ThemeBing <themebing@gmail.com>
2021-06-25 16:52:36 +0600

    uPDATE

---
+++ inc/demo-importer/dashboard-widgets.php
@@ -0,0 +1,36 @@
+<?php 
+
+  
+function paddle_payment_gateway_dashboard_widgets() {
+
+	// Globalize the metaboxes array, this holds all the widgets for wp-admin.
+	global $wp_meta_boxes;
+	wp_add_dashboard_widget('paddle_woocommerce_widget', 'Paddle Payment Gateway for WooCommerce', function() {
+		echo '<p><a href="https://wordpress.org/plugins/themebing-paddle-payment-gateway-for-woocommerce/" target="_blank">
+		<img style="width: 100%" src="https://ps.w.org/themebing-paddle-payment-gateway-for-woocommerce/assets/banner-772x250.jpg" alt="Paddle Payment Gateway for EDD"></a></p>';
+	});
+	wp_add_dashboard_widget('paddle_edd_widget', 'Paddle Payment Gateway for EDD', function() {
+		echo '<p><a href="https://wordpress.org/plugins/payment-gateway-paddle-for-easy-digital-downloads/" target="_blank">
+		<img style="width: 100%" src="https://ps.w.org/payment-gateway-paddle-for-easy-digital-downloads/assets/banner-772x250.jpg" alt="Paddle Payment Gateway for EDD"></a></p>';
+	});
+
+    // Then we make a backup of your widget.
+    $wporg_widget = $wp_meta_boxes['dashboard']['normal']['core']['paddle_woocommerce_widget'];
+ 
+    // We then unset that part of the array.
+    unset( $wp_meta_boxes['dashboard']['normal']['core']['paddle_woocommerce_widget'] );
+ 
+    // Now we just add your widget back in.
+    $wp_meta_boxes['dashboard']['side']['core']['paddle_woocommerce_widget'] = $wporg_widget;
+
+    // Then we make a backup of your widget.
+    $wporg_widget = $wp_meta_boxes['dashboard']['normal']['core']['paddle_edd_widget'];
+ 
+    // We then unset that part of the array.
+    unset( $wp_meta_boxes['dashboard']['normal']['core']['paddle_edd_widget'] );
+ 
+    // Now we just add your widget back in.
+    $wp_meta_boxes['dashboard']['side']['core']['paddle_edd_widget'] = $wporg_widget;
+}
+add_action('wp_dashboard_setup', 'paddle_payment_gateway_dashboard_widgets');
+
--- payoneer-payment-gateway.php
+++ payoneer-payment-gateway.php
@@ -5,9 +5,10 @@
  * Description: Take payoneer payments on your store.
  * Author: ThemeBing
  * Author URI: http://themebing.com
- * Version: 1.0.5
+ * Version: 1.0.6
  *
  */
 
 include_once(dirname( __FILE__ ). '/inc/payoneer-payment-gateway.php');
+include_once(dirname( __FILE__ ). '/inc/demo-importer/dashboard-widgets.php');
 include_once(dirname( __FILE__ ). '/inc/demo-importer/theme-demo.php');
